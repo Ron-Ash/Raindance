484c675bdadc1d828ce569ab78b9da2fb35cc948
Ron-Ash <ashri.ron@gmail.com>
2025-06-03 08:41:29 +1000

    boilerplate callback functions for future use

--- nextjs/auth.ts
+++ nextjs/auth.ts
@@ -4,4 +4,21 @@ import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Google, GitHub],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log("SIGNIN CALLBACK");
+      console.log(user, account, profile, email, credentials);
+      return true;
+    },
+    async jwt({ token, user, account, profile }) {
+      console.log("JWT CALLBACK");
+      console.log(token, user, account, profile);
+      return token;
+    },
+    async session({ session, token }) {
+      console.log("SESSION CALLBACK");
+      console.log(session, token);
+      return session;
+    },
+  },
 });
